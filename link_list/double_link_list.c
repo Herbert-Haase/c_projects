68f18b0b93948fd795c37df992f8c1f1e42d80b0
Herbert-Haase <herberthaase02@gmx.de>
2025-04-20 18:38:51 +0200

    clean up part2

---
+++ link_list/double_link_list.c
@@ -0,0 +1,133 @@
+// double_link_list.c
+#include "double_link_list.h"
+#include "link_list.h"
+int doublell_create(DoubleLL **list) {
+  if (!list)
+    return 1;
+  if (!*list) {
+    *list = calloc(1, sizeof(DoubleLL));
+    OUTOFMEMORY(list);
+  }
+  return 0;
+}
+
+DoubleNode *doublenode_create(int value) {
+  DoubleNode *node = malloc(sizeof(DoubleNode));
+  OUTOFMEMORY(node);
+  node->value = value;
+  node->next = NULL;
+  node->prev = NULL;
+  return node;
+}
+
+void doublell_append(DoubleLL *list, int value) {
+  if (!list)
+    return;
+  DoubleNode *new_node = doublenode_create(value);
+  if (list->size == 0) {
+    list->head = new_node;
+    list->tail = new_node;
+  } else {
+    new_node->prev = list->tail;
+    list->tail->next = new_node;
+    list->tail = new_node;
+  }
+  list->size++;
+}
+
+void doublell_prepend(DoubleLL *list, int value) {
+  if (!list)
+    return;
+  DoubleNode *new_node = doublenode_create(value);
+  if (list->size == 0) {
+    list->head = new_node;
+    list->tail = new_node;
+  } else {
+    new_node->next = list->head;
+    list->head->prev = new_node;
+    list->head = new_node;
+  }
+  list->size++;
+}
+
+void doublell_insert_at(DoubleLL *list, int idx, int value) {
+  if (!list || idx >= list->size)
+    return;
+  DoubleNode *p;
+  DoubleNode *new_node = doublenode_create(value);
+  if (idx <= (list->size / 2)) {
+    p = list->head;
+    for (int i = 0; i < idx; i++) {
+      p = p->next;
+    }
+    new_node->next = p;
+    if (p->prev) {
+      p->prev->next = new_node;
+      new_node->prev = p->prev;
+    } else {
+      list->head = new_node;
+    }
+    p->prev = new_node;
+  } else {
+    p = list->tail;
+    for (int i = 0; i < (list->size - idx) - 1; i++) {
+      p = p->prev;
+    }
+    new_node->next = p;
+    p->prev->next = new_node;
+    new_node->prev = p->prev;
+    p->prev = new_node;
+  }
+  list->size++;
+}
+
+void doublell_replace_at(DoubleLL *list, int idx, int value) {
+  if (!list || idx >= list->size)
+    return;
+  DoubleNode *p;
+  if (idx <= (list->size / 2)) {
+    p = list->head;
+    for (int i = 0; i < idx; i++) {
+      p = p->next;
+    }
+    p->value = value;
+  } else {
+    p = list->tail;
+    for (int i = 0; i <= (list->size - idx); i++) {
+      p = p->prev;
+    }
+    p->value = value;
+  }
+}
+
+void doublell_print(const DoubleLL *list) {
+  printf("Double Linked List: front ");
+  DoubleNode *p = list->head;
+  while (p) {
+    printf("%d ", p->value);
+    p = p->next;
+  }
+  printf("(size=%d)\n", list->size);
+  printf("Double Linked List: back ");
+  p = list->tail;
+  while (p) {
+    printf("%d ", p->value);
+    p = p->prev;
+  }
+  printf("(size=%d)\n", list->size);
+}
+
+void doublell_clear(DoubleLL *list) {
+  if (!list)
+    return;
+  DoubleNode *p = list->head;
+  while (p) {
+    DoubleNode *next = p->next;
+    free(p);
+    p = next;
+  }
+  list->head = NULL;
+  list->tail = NULL;
+  list->size = 0;
+}
+
--- stack/stack.h
+++ stack/stack.h
@@ -3,6 +3,8 @@
 #define STACK_H
 
 #include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 #define SIZE 32
 
@@ -10,16 +12,26 @@ typedef struct Stack {
   int arr[SIZE];
   int size;
   int max_size;
-  void (*push)(struct Stack*, int);
-  int (*pop)(struct Stack*);
-  int (*peek)(struct Stack*);
-  bool (*isempty)(struct Stack*);
-  bool (*isfull)(struct Stack*);
-  void (*clear)(struct Stack*);
-  void (*print_stack)(struct Stack*);
+  void (*push)(struct Stack *, int);
+  int (*pop)(struct Stack *);
+  int (*peek)(struct Stack *);
+  bool (*isempty)(struct Stack *);
+  bool (*isfull)(struct Stack *);
+  void (*clear)(struct Stack *);
+  void (*print_stack)(struct Stack *);
 } Stack;
 
-Stack* init_stack(void);
+void push(Stack *, int);
+int pop(Stack *);
+int peek(Stack *);
+int size(Stack *);
+bool isempty(Stack *);
+bool isfull(Stack *);
+Stack *init_stack(void);
+void clear(Stack *);
+void print_stack(Stack *);
+static void stack_overflow(void);
+static void stack_underflow(void);
+Stack *init_stack(void);
 
 #endif
-
