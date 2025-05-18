ebacbd88dd0a4147f03fb84474eeee2f0508c80b
Herbert-Haase <herberthaase02@gmx.de>
2025-05-18 23:04:23 +0200

    add cwd at the start and cd

--- custom_shell/shell.c
+++ custom_shell/shell.c
@@ -12,30 +12,44 @@
 int main(void) {
   puts("Welcome to mini shell");
   while (1) {
-    printf("$ ");
+    char cwd[MAX_LINE];
+    if (!getcwd(cwd, MAX_LINE)) {
+      perror("getcwd");
+      exit(EXIT_FAILURE);
+    }
+    printf("(%s) $ ", cwd);
     fflush(stdout);
+    char line[MAX_LINE];
+    char *args[MAX_ARGS];
+    int i = 0;
+    fflush(stdout);
+    if (!fgets(line, sizeof(line), stdin)) {
+      perror("fgets");
+      exit(EXIT_FAILURE);
+    }
+    line[strcspn(line, "\n")] = 0;
+
+    char *token = strtok(line, " ");
+    while (token != NULL && i < MAX_ARGS - 1) {
+      args[i++] = token;
+      token = strtok(NULL, " ");
+    }
+    args[i] = NULL;
+
+    if (strcmp(args[0], "cd") == 0) {
+      if (!args[1]) {
+        chdir("$HOME");
+      } else {
+        chdir(args[1]);
+      }
+      continue;
+    }
 
     pid_t cpid = fork();
     if (cpid == -1) {
       perror("fork");
       exit(EXIT_FAILURE);
     } else if (cpid == 0) {
-      char line[MAX_LINE];
-      char *args[MAX_ARGS];
-      int i = 0;
-      fflush(stdout);
-      if (!fgets(line, sizeof(line), stdin)) {
-        perror("fgets");
-        _exit(EXIT_FAILURE);
-      }
-      line[strcspn(line, "\n")] = 0;
-
-      char *token = strtok(line, " ");
-      while (token != NULL && i < MAX_ARGS - 1) {
-        args[i++] = token;
-        token = strtok(NULL, " ");
-      }
-      args[i] = NULL;
 
       execvp(args[0], args);
       perror("execvp");
